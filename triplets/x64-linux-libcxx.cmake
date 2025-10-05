d964be6cd822de344ef3de4eb983b70210c4f044
Sergio Martins <@>
2025-10-05 11:08:29 +0100

    Add libc++ triplet

---
+++ triplets/x64-linux-libcxx.cmake
@@ -0,0 +1,9 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(VCPKG_C_FLAGS "")
+string(APPEND VCPKG_CXX_FLAGS " -stdlib=libc++")
+string(APPEND VCPKG_LINKER_FLAGS " -stdlib=libc++")
