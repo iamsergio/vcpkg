40f4f556d729dec87f4c7ad4c4dd72df30fa89ec
Sergio Martins <sergio.martins@kdab.com>
2026-02-19 20:21:26 +0000

    Add my sanitizer triplets

---
+++ custom_triplets/x64-linux-tsan.cmake
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2025 Sergio Martins
+#
+# SPDX-License-Identifier: MIT
+
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(VCPKG_C_FLAGS "-fsanitize=thread")
+set(VCPKG_CXX_FLAGS "-fsanitize=thread")
+set(VCPKG_LINKER_FLAGS "-fsanitize=thread")
